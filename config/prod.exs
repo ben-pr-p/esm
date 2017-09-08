248eae650113401f0c222f1b41f31d09722c36fe
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-09-08 16:54:19 -0400

    use system tuple for hostname

--- config/prod.exs
+++ config/prod.exs
@@ -29,7 +29,7 @@ config :osdi, Osdi.Repo,
   database: System.get_env("RDS_DB_NAME"),
   username: System.get_env("RDS_DB_USER"),
   password: System.get_env("RDS_DB_PASSWORD"),
-  hostname: System.get_env("RDS_HOST"),
+  hostname: {:system, "RDS_HOST"},
   port: "5432"
 
 # ## SSL Support
