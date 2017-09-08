a87e0b1ee4e1ac7db9999e3ad92bbc377abd3f72
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-09-08 18:04:52 -0400

    server true

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :admin, Admin.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "admin.justicedemocrats.com", port: 4000],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
