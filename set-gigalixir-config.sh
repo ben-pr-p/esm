81b9eb65fb4deb4dee628ef0e1aab1cf264f0970
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-01-18 22:24:34 -0500

    merge master - new configuration strategy

--- set-gigalixir-config.sh
+++ set-gigalixir-config.sh
@@ -4,5 +4,5 @@ for var in $vars
 do
   splitvar=($(echo $var | tr '=' "\n"))
   gigalixir set_config jd-livevox ${splitvar[0]} ${splitvar[1]}
-  echo "gigalixir set_config admin ${splitvar[0]} ${splitvar[1]}"
+  echo "gigalixir set_config beto-esm ${splitvar[0]} ${splitvar[1]}"
 done
