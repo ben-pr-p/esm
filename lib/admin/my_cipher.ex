3cfae6578849afbd7eea54064d3a4cc6a4cffaf1
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-01-30 16:50:29 -0500

    more descriptive logs

--- lib/admin/my_cipher.ex
+++ lib/admin/my_cipher.ex
@@ -1,8 +1,12 @@
 defmodule MyCipher do
   def decrypt(string) do
     case Cipher.decrypt(string) do
-      "" -> Admin.OldSecrets.decrypt(string) |> IO.inspect()
-      decrypted -> decrypted
+      "" ->
+        IO.puts "cipher could not decrypt. must be old secret"
+        Admin.OldSecrets.decrypt(string) |> IO.inspect()
+      decrypted ->
+        IO.puts "cipher decrypted to #{decrypted}"
+        decrypted
     end
   end
 end
