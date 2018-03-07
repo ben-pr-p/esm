5d35565315452479ecc2ddfab3913d7684e37334
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-03-07 17:32:23 -0500

    limit date picker to after today

--- assets/js/components/editable-date-range.jsx
+++ assets/js/components/editable-date-range.jsx
@@ -51,7 +51,7 @@ export default class EditableDate extends Component {
   };
 
   handleClickOutside = () => {
-    this.props.checkin();
+    if (this.props.checkin) this.props.checkin();
     this.setState({ editing: false });
   };
 
@@ -75,7 +75,7 @@ export default class EditableDate extends Component {
       start_moment.clone().add(2, "hours");
 
     return (
-      <div onDoubleClick={this.editOn}>
+      <div onDoubleClick={this.editOn} style={{ cursor: "pointer" }}>
         <Modal
           title={`Edit ${friendly[this.props.attr]}`}
           visible={this.state.editing}
@@ -85,6 +85,7 @@ export default class EditableDate extends Component {
           <strong> Date: </strong>
           <DatePicker
             defaultValue={start_moment}
+            disabledDate={disabledDate}
             onChange={this.onDateChange}
           />
           <br />
@@ -134,4 +135,9 @@ export default class EditableDate extends Component {
       </div>
     );
   };
+}
+
+function disabledDate(current) {
+  // Can not select days before today and today
+  return current && current < moment().endOf("day");
 }
