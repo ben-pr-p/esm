cfe600b735a8da1c9db5e3e610314a21c3e3dfd8
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-05-24 15:02:39 -0400

    modified config changer

--- set-gigalixir-config.sh
+++ set-gigalixir-config.sh
@@ -3,6 +3,6 @@ vars=`cat .env`
 for var in $vars
 do
   splitvar=($(echo $var | tr '=' "\n"))
-  gigalixir set_config beto-esm ${splitvar[0]} ${splitvar[1]}
-  echo "gigalixir set_config beto-esm ${splitvar[0]} ${splitvar[1]}"
+  gigalixir set_config randy-esm ${splitvar[0]} ${splitvar[1]}
+  echo "gigalixir set_config randy-esm ${splitvar[0]} ${splitvar[1]}"
 done
